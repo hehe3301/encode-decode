45b5eb3006c9d434debe3a55f1a99418749d7e02
Alexander H <hehe3301@gmail.com>
2013-09-22 17:10:14 -0400

    Numerical conversion works
    the scripts now convert the user imputed message to a list of numbers
    that are linked to the letters

--- encoder.py
+++ encoder.py
@@ -1,8 +1,11 @@
 from numswap import *
 message=list(input('What mesage would you like to encode?'))
-l=list()
-
-
-
+code=list()
+i=0
+for i in range (0,len(message)):
+    code.append(letterswap(message[i]))
+    i=i+1
 
 print(message)
+print(code)
+
--- numswap.py
+++ numswap.py
@@ -3,85 +3,85 @@ def letterswap(letter):
     This function 
     '''
     if letter == 'a':
-        return '1'
+        return 1
     elif letter == 'b':
-        return '2'
+        return 2
     elif letter == 'c':
-        return '3'
+        return 3
     elif letter == 'd':
-        return '4'
+        return 4
     elif letter == 'e':
-        return '5'
+        return 5
     elif letter == 'f':
-        return '6'
+        return 6
     elif letter == 'g':
-        return '7'
+        return 7
     elif letter == 'h':
-        return '8'
+        return 8
     elif letter == 'i':
-        return '9'
+        return 9
     elif letter == 'j':
-        return '10'
+        return 10
     elif letter == 'k':
-        return '11'
+        return 11
     elif letter == 'l':
-        return '12'
+        return 12
     elif letter == 'm':
-        return '13'
+        return 13
     elif letter == 'n':
-        return '14'
+        return 14
     elif letter == 'o':
-        return '15'
+        return 15
     elif letter == 'p':
-        return '16'
+        return 16
     elif letter == 'q':
-        return '17'
+        return 17
     elif letter == 'r':
-        return '18'
+        return 18
     elif letter == 's':
-        return '19'
+        return 19
     elif letter == 't':
-        return '20'
+        return 20
     elif letter == 'u':
-        return '21'
+        return 21
     elif letter == 'v':
-        return '22'
+        return 22
     elif letter == 'w':
-        return '23'
+        return 23
     elif letter == 'x':
-        return '24'
+        return 24
     elif letter == 'y':
-        return '25'
+        return 25
     elif letter == 'z':
-        return '26'
+        return 26
     elif letter == '1':
-        return '27'
+        return 27
     elif letter == '2':
-        return '28'
+        return 28
     elif letter == '3':
-        return '29'
+        return 29
     elif letter == '4':
-        return '30'
+        return 30
     elif letter == '5':
-        return '31'
+        return 31
     elif letter == '6':
-        return '32'
+        return 32
     elif letter == '7':
-        return '33'
+        return 33
     elif letter == '8':
-        return '34'
+        return 34
     elif letter == '9':
-        return '35'
+        return 35
     elif letter == '0':
-        return '36'
+        return 36
     elif letter == '.':
-        return '37'
+        return 37
     elif letter == ' ':
-        return '38'
+        return 38
     elif letter == '?':
-        return '39'
+        return 39
     elif letter == ',':
-        return '40'
+        return 40
 
 def numberswap(letter):
     pass
